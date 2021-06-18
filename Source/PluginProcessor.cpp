86da8182381530554ba6285612c466ada0f6f595
Kanav khanna <Kanav.k@hotmail.com>
2021-06-18 12:11:58 +0530

    Audio Parameters

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -12,14 +12,14 @@
 //==============================================================================
 JuiceAudioEQAudioProcessor::JuiceAudioEQAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
+    : AudioProcessor(BusesProperties()
+#if !JucePlugin_IsMidiEffect
+#if !JucePlugin_IsSynth
+                         .withInput("Input", juce::AudioChannelSet::stereo(), true)
+#endif
+                         .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+      )
 #endif
 {
 }
@@ -36,29 +36,29 @@ const juce::String JuiceAudioEQAudioProcessor::getName() const
 
 bool JuiceAudioEQAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
+#if JucePlugin_WantsMidiInput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool JuiceAudioEQAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
+#if JucePlugin_ProducesMidiOutput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool JuiceAudioEQAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
+#if JucePlugin_IsMidiEffect
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 double JuiceAudioEQAudioProcessor::getTailLengthSeconds() const
@@ -68,8 +68,8 @@ double JuiceAudioEQAudioProcessor::getTailLengthSeconds() const
 
 int JuiceAudioEQAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    return 1; // NB: some hosts don't cope very well if you tell them there are 0 programs,
+              // so this should be at least 1, even if you're not really implementing programs.
 }
 
 int JuiceAudioEQAudioProcessor::getCurrentProgram()
@@ -77,21 +77,21 @@ int JuiceAudioEQAudioProcessor::getCurrentProgram()
     return 0;
 }
 
-void JuiceAudioEQAudioProcessor::setCurrentProgram (int index)
+void JuiceAudioEQAudioProcessor::setCurrentProgram(int index)
 {
 }
 
-const juce::String JuiceAudioEQAudioProcessor::getProgramName (int index)
+const juce::String JuiceAudioEQAudioProcessor::getProgramName(int index)
 {
     return {};
 }
 
-void JuiceAudioEQAudioProcessor::changeProgramName (int index, const juce::String& newName)
+void JuiceAudioEQAudioProcessor::changeProgramName(int index, const juce::String &newName)
 {
 }
 
 //==============================================================================
-void JuiceAudioEQAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void JuiceAudioEQAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
     // Use this method as the place to do any pre-playback
     // initialisation that you need..
@@ -104,35 +104,34 @@ void JuiceAudioEQAudioProcessor::releaseResources()
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool JuiceAudioEQAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+bool JuiceAudioEQAudioProcessor::isBusesLayoutSupported(const BusesLayout &layouts) const
 {
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
+#if JucePlugin_IsMidiEffect
+    juce::ignoreUnused(layouts);
     return true;
-  #else
+#else
     // This is the place where you check if the layout is supported.
     // In this template code we only support mono or stereo.
     // Some plugin hosts, such as certain GarageBand versions, will only
     // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
+        // This checks if the input layout matches the output layout
+#if !JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
-   #endif
+#endif
 
     return true;
-  #endif
+#endif
 }
 #endif
 
-void JuiceAudioEQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+void JuiceAudioEQAudioProcessor::processBlock(juce::AudioBuffer<float> &buffer, juce::MidiBuffer &midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumInputChannels = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
     // In case we have more outputs than inputs, this code clears any output
@@ -142,7 +141,7 @@ void JuiceAudioEQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     // when they first compile a plugin, but obviously you don't need to keep
     // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+        buffer.clear(i, 0, buffer.getNumSamples());
 
     // This is the place where you'd normally do the guts of your plugin's
     // audio processing...
@@ -152,7 +151,7 @@ void JuiceAudioEQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     // interleaved by keeping the same state.
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
-        auto* channelData = buffer.getWritePointer (channel);
+        auto *channelData = buffer.getWritePointer(channel);
 
         // ..do something to the data...
     }
@@ -164,28 +163,78 @@ bool JuiceAudioEQAudioProcessor::hasEditor() const
     return true; // (change this to false if you choose to not supply an editor)
 }
 
-juce::AudioProcessorEditor* JuiceAudioEQAudioProcessor::createEditor()
+juce::AudioProcessorEditor *JuiceAudioEQAudioProcessor::createEditor()
 {
-    return new JuiceAudioEQAudioProcessorEditor (*this);
+    //return new JuiceAudioEQAudioProcessorEditor(*this);
+    return new juce::GenericAudioProcessorEditor(AudioProcessor * p);
+    //the the actual changes visable in the gui
+    )
 }
 
 //==============================================================================
-void JuiceAudioEQAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+void JuiceAudioEQAudioProcessor::getStateInformation(juce::MemoryBlock &destData)
 {
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
 }
 
-void JuiceAudioEQAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void JuiceAudioEQAudioProcessor::setStateInformation(const void *data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
 }
 
+juce::AudioProcessorValueTreeState::ParameterLayout
+SimpleEQAudioProcessor::createParameterLayout() //code for equalizer
+//  gonna keep 3 bands for now low, high, parametric/peak
+//cut bands: controllable frequency/slope
+//parametric bands:controllable frequency, gain, quality
+{
+    juce::AudioProcessorValueTreeState::ParameterLayout layout;
+
+    layout.add(std::make_unique<juce::AudioParameterFloat>("LowCut Freq",
+                                                           "LowCut Freq",
+                                                           juce::NormalizableRange<float>(20.f, 20000.f, 1.f, 1.f), 20.f));
+
+    //3rd here is normalizable range
+
+    layout.add(std::make_unique<juce::AudioParameterFloat>("HighCut Freq",
+                                                           "HighCut Freq",
+                                                           juce::NormalizableRange<float>(20.f, 20000.f, 1.f, 1.f), 20000.f));
+
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Freq",
+                                                           "Peak Freq",
+                                                           juce::NormalizableRange<float>(20.f, 20000.f, 1.f, 1.f), 750.f));
+
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Gain",
+                                                           "Peak Gain",
+                                                           juce::NormalizableRange<float>(-24.f, 24.f, 0.5f, 1.f), 0.0f));
+
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Quality",
+                                                           "Peak Quality",
+                                                           juce::NormalizableRange<float>(0.1f, 10.f, 0.05f, 1.f), 1.0f)); //3rd parameter here is
+    step size
+
+        juice::StringArray stringArray for (int i = 0; i < 4; ++i)
+    {
+        juice::string str;
+        str << (12 + i * 12);
+        str << "db/oct";
+        stringArray.add(str);
+    }
+
+    layout.add(std::make_unique<juce::AudioParameterChoice>("LowCut Slope", "LowCut Slope", StringArray, 0));
+    layout.add(std::make_unique<juce::AudioParameterChoice>("HighCut Slope", "HighCut Slope", StringArray, 0));
+
+    juce::AudioParameterChoice()
+
+        return layout;
+}
+
 //==============================================================================
 // This creates new instances of the plugin..
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+juce::AudioProcessor *JUCE_CALLTYPE createPluginFilter()
 {
     return new JuiceAudioEQAudioProcessor();
 }
